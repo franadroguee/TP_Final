75c41b5024aab9a36b277e392caf83b7354facdd
franadroguee <fadrogue@udesa.edu.ar>
2026-05-18 13:50:18 -0300

    improved mapping

--- mapa.py
+++ mapa.py
@@ -5,37 +5,52 @@ pantalla = pygame.display.set_mode((930, 840))
 playing = True
 clock = pygame.time.Clock()
 
+# graficos
 pared = pygame.image.load('graficos\pared.png')
 pasillo = pygame.image.load('graficos\pasillo.png')
 power = pygame.image.load('graficos\powerpellet.png')
 puerta = pygame.image.load('graficos\puerta.png')
 punto = pygame.image.load('graficos\punto.png')
 
-while playing:
+
+def mapa(ruta_archivo: str) -> dict:
     x = 0
     y = 0
-    with open('mapa.txt', 'r') as mapa:
+    dic_mapa = {}
+    with open(ruta_archivo, 'r') as mapa:
         for fila in mapa:
             for letra in fila:
                 if letra == 'X':
-                    pantalla.blit(pared, (x, y))
+                    pantalla.blit(pared, ((x * 30), (y * 30)))
+                    dic_mapa[(x, y)] = 'pared'
                 elif letra == '.':
-                    pantalla.blit(punto, (x, y))
+                    pantalla.blit(punto, ((x * 30), (y * 30)))
+                    dic_mapa[(x, y)] = 'punto'
                 elif letra == ' ':
-                    pantalla.blit(pasillo, (x, y))
+                    pantalla.blit(pasillo, ((x * 30), (y * 30)))
+                    dic_mapa[(x, y)] = 'pasillo'
                 elif letra == 'G':
-                    pantalla.blit(pasillo, (x, y))
+                    pantalla.blit(pasillo, ((x * 30), (y * 30)))
+                    dic_mapa[(x, y)] = 'ghost'
                 elif letra == 'o':
-                    pantalla.blit(power, (x, y))
+                    pantalla.blit(power, ((x * 30), (y * 30)))
+                    dic_mapa[(x, y)] = 'powerpellet'
                 elif letra == '-':
-                    pantalla.blit(puerta, (x, y))
+                    pantalla.blit(puerta, ((x * 30), (y * 30)))
+                    dic_mapa[(x, y)] = 'puerta'
                 elif letra == 'T':
-                    pantalla.blit(puerta, (x, y))
-                x += 30
+                    pantalla.blit(puerta, ((x * 30), (y * 30)))
+                    dic_mapa[(x, y)] = 'tunel'
+                elif letra == 'P':
+                    dic_mapa[(x, y)] = 'inicio'
+                x += 1
             x = 0
-            y += 30
+            y += 1
     pygame.display.update()
-            
+    return dic_mapa
+
+while playing:
+    dic_mapa = mapa('mapa.txt')  
     for event in pygame.event.get():    
         if event.type == pygame.QUIT:
             pygame.quit()
